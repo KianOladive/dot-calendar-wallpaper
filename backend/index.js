70fe06c42fc23292e8c66a2912a9dbf7f62b6382
Kian Oladive <kian.oladive@student.ateneo.edu>
2026-08-07 12:51:08 +0800

    make backend/index.js read process port
    defaults to 3000 still

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import app from './app.js';
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
