a30f71ee334c6fefef0c48e04832ef753ad198a7
Kian Oladive <kian.oladive@student.ateneo.edu>
2026-08-10 23:30:13 +0800

    serve frontend from backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import imageRouter from './routes/image-route.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const distPath = path.join(__dirname, '../frontend/dist');
 
 const app = express();
 
 app.use(express.json());
 app.use('/goal', imageRouter);
 
+app.use(express.static(distPath));
+
 app.use((_, res) => {
-  res.status(404).json({ "message": "non-existent" });
-})
+  res.sendFile(path.join(distPath, 'index.html'));
+});
 
 export default app;
